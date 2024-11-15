755b80032b8c21fdcd8561b28e87e9849a3db292
purwinal <purwinal@gmail.com>
2024-11-15 14:08:12 -0500

    modified responsiveness

--- src/App.js
+++ src/App.js
@@ -246,6 +246,17 @@ function App() {
         }
     };
 
+    const setViewportHeight = () => {
+        const vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+
+    useEffect(() => {
+        setViewportHeight();
+        window.addEventListener('resize', setViewportHeight);
+        return () => window.removeEventListener('resize', setViewportHeight);
+    }, []);
+
   	return (
         <div className={styles.gridContainer}>
             <div className={`${styles.topSection} ${isMenuOpen ? styles.menuOpen : ""}`}>
